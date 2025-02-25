042c7e169e12ee68b393fc7f147ef50437233183
Tolga <mail@tolgadur.com>
2025-02-25 16:58:16 +0000

    config change

--- src/config.py
+++ src/config.py
@@ -5,6 +5,7 @@ MODEL_NAME = "openai/whisper-tiny"
 
 PROCESSOR = WhisperProcessor.from_pretrained(MODEL_NAME)
 MODEL = WhisperForConditionalGeneration.from_pretrained(MODEL_NAME)
+MODEL.config.forced_decoder_ids = None
 
 DEVICE = "mps" if torch.backends.mps.is_available() else "cpu"
 if torch.cuda.is_available():
