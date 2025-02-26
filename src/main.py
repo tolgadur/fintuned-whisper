331ffd77452076b180eef836dc7b08cf2f30241e
Tolga <mail@tolgadur.com>
2025-02-26 16:07:32 +0000

    new out of sample logs

--- src/main.py
+++ src/main.py
@@ -8,29 +8,47 @@ def main():
     # train(num_epochs=5)
 
     # print("Training with LoRA and KL divergence...")
-    train_lora_kd(num_epochs=5)
+    # train_lora_kd(num_epochs=5)
 
     # Comment out the following line to evaluate the single datapoint
     # evaluate_single_datapoint(model_path="models/whisper-tiny-librispeech.pth")
     # evaluate_single_datapoint(model_path="models/whisper-tiny-lora-kd")
 
-    # print("Evaluating...")
-    # print("Evaluating regular non-fined tuned model")
-    # evaluate_out_of_sample()
-    # print("-" * 100)
+    print("Evaluating...")
+    print("Evaluating regular non-fined tuned model")
+    evaluate_out_of_sample()
+    print("-" * 100)
 
-    # print("Evaluating model finetuned with one epoch")
-    # evaluate_out_of_sample(model_path="models/whisper-tiny-librispeech-epoch-1.pth")
-    # print("-" * 100)
+    print("Evaluating model finetuned with one epoch")
+    evaluate_out_of_sample(model_path="models/whisper-tiny-librispeech-epoch-1.pth")
+    print("-" * 100)
 
-    # print("Evaluating model finetuned with five epochs")
-    # evaluate_out_of_sample(model_path="models/whisper-tiny-librispeech-epoch-4.pth")
-    # print("-" * 100)
+    print("Evaluating model finetuned with five epochs")
+    evaluate_out_of_sample(model_path="models/whisper-tiny-librispeech-epoch-4.pth")
+    print("-" * 100)
 
-    # Uncomment to evaluate LoRA KD model if it exists
-    # print("Evaluating model finetuned with LoRA and KL divergence")
-    # evaluate_out_of_sample(model_path="models/whisper-tiny-lora-kd")
-    # print("-" * 100)
+    print("Evaluating model finetuned with LoRA and KL divergence one epochs")
+    evaluate_out_of_sample(model_path="models/whisper-tiny-lora-kd/checkpoint-1784")
+    print("-" * 100)
+
+    print("Evaluating model finetuned with LoRA and KL divergence two epochs")
+    evaluate_out_of_sample(model_path="models/whisper-tiny-lora-kd/checkpoint-3568")
+    print("-" * 100)
+
+    print("Evaluating model finetuned with LoRA and KL divergence three epochs")
+    evaluate_out_of_sample(model_path="models/whisper-tiny-lora-kd/checkpoint-5352")
+    print("-" * 100)
+
+    print("Evaluating model finetuned with LoRA and KL divergence four epochs")
+    evaluate_out_of_sample(model_path="models/whisper-tiny-lora-kd/checkpoint-7136")
+    print("-" * 100)
+
+    print("Evaluating model finetuned with LoRA and KL divergence five epochs")
+    evaluate_out_of_sample(model_path="models/whisper-tiny-lora-kd/checkpoint-8920")
+
+    print("Evaluating model finetuned with LoRA and KL divergence five epochs again")
+    evaluate_out_of_sample(model_path="models/whisper-tiny-lora-kd")
+    print("-" * 100)
 
 
 if __name__ == "__main__":
